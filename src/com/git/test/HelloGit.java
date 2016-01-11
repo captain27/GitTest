12b87ada808708e23b858737318dd3ab4a72547d
ZhouWeitao <memory-zwt@qq.com>
2016-01-12 07:58:51 +0800

    第二次测试Git

--- src/com/git/test/HelloGit.java
+++ src/com/git/test/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("hello world");
+        System.out.println("第二次测试");
     }
 }
